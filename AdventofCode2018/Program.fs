6aed27ef14955e7e8573d6abfd25b904e6f0918e
Kurt Boberg <amroukithkin@gmail.com>
2018-12-17 14:42:57 -0800

    Day 3 solutions

--- AdventofCode2018/Program.fs
+++ AdventofCode2018/Program.fs
@@ -65,6 +65,63 @@ let solution2b (input : string[]) =
     |> List.head
     |> (fun x -> (fst x).Replace("_",""))
 
+let extractCoordinates (input: string) = 
+    
+    let rect = input.Split(':') |> Array.map (fun x -> x.Trim())
+    let topleft_anchors = rect |> Array.head |> (fun x -> x.Split('@')) |> Array.map (fun x -> x.Trim())
+    let ax = topleft_anchors |> Array.last |> (fun x -> x.Split(',')) |> Array.head |> int
+    let ay = topleft_anchors |> Array.last |> (fun x -> x.Split(',')) |> Array.last |> int
+    let rx = rect |> Array.last |> (fun x -> x.Split('x')) |> Array.head |> int
+    let ry = rect |> Array.last |> (fun x -> x.Split('x')) |> Array.last |> int
+    let claim = topleft_anchors |> Array.head
+
+    let sequence = seq {for x in 0..rx-1 do for y in 0..ry-1 do yield ((ax + x, ay + y), claim)}
+    sequence
+
+
+let solution3a (input: string[]) =
+
+    let sharedCoordinates = 
+        input
+        |> Array.map(extractCoordinates)
+        |> Array.reduce Seq.append
+        |> Seq.countBy(fst)
+        |> Seq.filter(fun x -> (snd x) > 1)
+    
+    let sharedCount = 
+        sharedCoordinates
+        |> Seq.length
+    
+    string sharedCount
+
+let solution3b (input: string[]) =
+
+    let claimnumbers = 
+        input 
+        |> Array.map(fun x -> x.Split('@').[0].Trim())
+        |> Set
+
+    let claims =
+        input
+        |> Array.map(extractCoordinates)
+        |> Array.reduce Seq.append
+    
+    let multiclaims = 
+        claims
+        |> Seq.groupBy(fst)
+        |> Seq.filter(fun x -> (snd x) |> Seq.length > 1)
+        |> Seq.collect(snd)
+        |> Seq.map(snd)
+        |> Set
+        
+    let singleClaim =
+        Set.difference claimnumbers multiclaims
+        |> Set.toSeq
+
+    singleClaim
+    |> Seq.exactlyOne
+
+
 [<EntryPoint>]
 let main argv = 
     printfn "Select challenge: "
@@ -84,6 +141,14 @@ let main argv =
             File.ReadAllLines("2b_test.txt") |> solution2b
         | "2b" ->
             File.ReadAllLines("solution2_input.txt") |> solution2b
+        | "3a_test" ->
+            File.ReadAllLines("3a_test.txt" ) |> solution3a
+        | "3a" -> 
+            File.ReadAllLines("solution3_input.txt") |> solution3a
+        | "3b_test" ->
+            File.ReadAllLines("3a_test.txt") |> solution3b
+        | "3b" ->
+            File.ReadAllLines("solution3_input.txt") |> solution3b
         | _ -> "bad arg"
     printfn "Solution : %s" solution
     Console.ReadLine() |> ignore
